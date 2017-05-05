f0134e997bd8da21e6b3165b9f6c5302eba895bf
Tom Smith <tsmith@ebi.ac.uk>
2017-05-05 14:52:49 +0100

    Reformatted running query

--- count_profiling.py
+++ count_profiling.py
@@ -10,6 +10,33 @@ from pymongo import MongoClient, ReadPreference
 import numpy as np
 from scipy import stats
 
+CHROM_INFO = {
+    "1": {"minStart": 10020, "maxStart": 249240605, "numEntries": 12422239},
+    "2": {"minStart": 10133, "maxStart": 243189190, "numEntries": 13217397},
+    "3": {"minStart": 60069, "maxStart": 197962381, "numEntries": 10891260},
+    "4": {"minStart": 10006, "maxStart": 191044268, "numEntries": 10427984},
+    "5": {"minStart": 10043, "maxStart": 180905164, "numEntries": 9742153},
+    "6": {"minStart": 61932, "maxStart": 171054104, "numEntries": 9340928},
+    "7": {"minStart": 10010, "maxStart": 159128653, "numEntries": 8803393},
+    "8": {"minStart": 10059, "maxStart": 146303974, "numEntries": 8458842},
+    "9": {"minStart": 10024, "maxStart": 141153428, "numEntries": 6749462},
+    "10": {"minStart": 60222, "maxStart": 135524743, "numEntries": 7416994},
+    "11": {"minStart": 61248, "maxStart": 134946509, "numEntries": 7690584},
+    "12": {"minStart": 60076, "maxStart": 133841815, "numEntries": 7347630},
+    "13": {"minStart": 19020013, "maxStart": 115109865, "numEntries": 5212835},
+    "14": {"minStart": 19000005, "maxStart": 107289456, "numEntries": 4989875},
+    "15": {"minStart": 20000003, "maxStart": 102521368, "numEntries": 4607392},
+    "16": {"minStart": 60008, "maxStart": 90294709, "numEntries": 5234679},
+    "17": {"minStart": 47, "maxStart": 81195128, "numEntries": 4652428},
+    "18": {"minStart": 10005, "maxStart": 78017157, "numEntries": 4146560},
+    "19": {"minStart": 60360, "maxStart": 59118925, "numEntries": 3821659},
+    "20": {"minStart": 60039, "maxStart": 62965384, "numEntries": 3512381},
+    "21": {"minStart": 9411199, "maxStart": 48119868, "numEntries": 2082680},
+    "22": {"minStart": 16050036, "maxStart": 51244515, "numEntries": 2172028},
+    "X": {"minStart": 60003, "maxStart": 155260479, "numEntries": 5893713},
+    "Y": {"minStart": 10003, "maxStart": 59363485, "numEntries": 504508}
+}
+
 
 def main():
     parser = ArgParser(sys.argv)
@@ -35,69 +62,91 @@ def benchmark(collection):
     find_count_ex_times = []
     aggregate_count_ex_times = []
 
+    find_count_counts = []
+    aggregate_count_counts = []
+
     random.shuffle(benchmark_functions)
+
+    print(
+        "n_iters: {}, numRuns: {}, chrom: {}, minChromPos: {}, maxChromPos: {}, margin: {}, query_length: {}"
+        .format(n_iters, numRuns, chrom, minChromPos, maxChromPos, margin, query_length))
 
     for _ in bar(list(range(n_iters))):
 
         benchmark_functions.insert(1, benchmark_functions.pop(0))
 
         for benchmark_function in benchmark_functions:
-            new_ex_times = benchmark_function(numRuns, minChromPos, maxChromPos, query_length, chrom, margin, collection)
+            new_ex_times, new_counts = benchmark_function(numRuns, minChromPos, maxChromPos,
+                                                          query_length, chrom, margin, collection)
             if benchmark_function == benchmark_find_count:
                 find_count_ex_times.extend(new_ex_times)
+                find_count_counts.extend(new_counts)
             elif benchmark_function == benchmark_aggregate_count:
                 aggregate_count_ex_times.extend(new_ex_times)
+                aggregate_count_counts.extend(new_counts)
             else:
                 print(benchmark_function)
                 sys.exit(1)
 
-        output_stats(find_count_ex_times, aggregate_count_ex_times)
+        output_stats(find_count_ex_times, find_count_counts, aggregate_count_ex_times,
+                     aggregate_count_counts)
 
 
-
-
-def output_stats(find_count_ex_times, aggregate_count_ex_times):
-    find_count_mean = np.mean(find_count_ex_times)
-    aggregate_count_mean = np.mean(aggregate_count_ex_times)
-
-    find_count_quartiles = [
-        min(find_count_ex_times),
-        np.percentile(find_count_ex_times, 25),
-        np.percentile(find_count_ex_times, 50),
-        np.percentile(find_count_ex_times, 75),
-        max(find_count_ex_times)
-    ]
-
-    aggregate_count_quartiles = [
-        min(aggregate_count_ex_times),
-        np.percentile(aggregate_count_ex_times, 25),
-        np.percentile(aggregate_count_ex_times, 50),
-        np.percentile(aggregate_count_ex_times, 75),
-        max(aggregate_count_ex_times)
-    ]
-
-    find_count_std = np.std(find_count_ex_times)
-    aggregate_count_std = np.std(aggregate_count_ex_times)
-
+def output_stats(find_count_ex_times, find_count_counts, aggregate_count_ex_times,
+                 aggregate_count_counts):
     print("\n\n#####################################################\n")
     print("STATS\n")
 
-    print("find().count() execution times: {}".format(find_count_ex_times))
-    print("aggregate count execution times: {}".format(aggregate_count_ex_times))
+    print("EX TIMES STATS")
+    stats_helper(find_count_ex_times, aggregate_count_ex_times)
+
+    print("\nEX TIMES / RECORD COUNT STATS")
+    stats_helper(find_count_counts, aggregate_count_counts)
+
+    print("\n#####################################################\n\n")
+
+
+def stats_helper(find_count_stat_list, aggregate_count_stat_list):
+    find_count_mean = np.mean(find_count_stat_list)
+    aggregate_count_mean = np.mean(aggregate_count_stat_list)
+
+    find_count_quartiles = [
+        min(find_count_stat_list),
+        np.percentile(find_count_stat_list, 25),
+        np.percentile(find_count_stat_list, 50),
+        np.percentile(find_count_stat_list, 75),
+        max(find_count_stat_list)
+    ]
+
+    aggregate_count_quartiles = [
+        min(aggregate_count_stat_list),
+        np.percentile(aggregate_count_stat_list, 25),
+        np.percentile(aggregate_count_stat_list, 50),
+        np.percentile(aggregate_count_stat_list, 75),
+        max(aggregate_count_stat_list)
+    ]
+
+    find_count_std = np.std(find_count_stat_list)
+    aggregate_count_std = np.std(aggregate_count_stat_list)
+
+    print("find().count():\n{}".format(find_count_stat_list))
+    print("aggregate count:\n{}".format(aggregate_count_stat_list))
 
     print("\nStats [find_count, aggregate_count]\n")
     print("Means: {}, {}".format(find_count_mean, aggregate_count_mean))
     print("Quartiles:\n{}\n{}\n".format(find_count_quartiles, aggregate_count_quartiles))
     print("Standard deviation: {}, {}".format(find_count_std, aggregate_count_std))
 
-    print("T-test (t-statistic, two-tailed p-value): {}".format(stats.ttest_ind(find_count_ex_times,
-                                                                                aggregate_count_ex_times,
-                                                                                equal_var=False)))
-    print("\n#####################################################\n\n")
+    print(
+        "T-test (t-statistic, two-tailed p-value): {}".format(stats.ttest_ind(find_count_stat_list,
+                                                                              aggregate_count_stat_list,
+                                                                              equal_var=False)))
 
 
-def benchmark_find_count(numRuns, minChromPos, maxChromPos, query_length, chrom, margin, collection):
+def benchmark_find_count(numRuns, minChromPos, maxChromPos, query_length, chrom, margin,
+                         collection):
     ex_times = []
+    counts = []
     for _ in range(numRuns):
         # Use random positions for each run to avoid caching effects
         start = random.randint(minChromPos, maxChromPos)
@@ -107,16 +156,18 @@ def benchmark_find_count(numRuns, minChromPos, maxChromPos, query_length, chrom,
                  "start": {"$gt": start - margin, "$lte": end},
                  "end": {"$gte": start, "$lte": end + margin}}
 
-        startTime = datetime.datetime.now()
-        collection.find(query).count()
-        endTime = datetime.datetime.now()
-        ex_times.append((endTime - startTime).total_seconds())
+        ex_time, count = run_query("find_count", collection, query)
 
-    return ex_times
+        counts.append(count)
+        ex_times.append(ex_time)
+
+    return ex_times, counts
 
 
-def benchmark_aggregate_count(numRuns, minChromPos, maxChromPos, query_length, chrom, margin, collection):
+def benchmark_aggregate_count(numRuns, minChromPos, maxChromPos, query_length, chrom, margin,
+                              collection):
     ex_times = []
+    counts = []
     for _ in range(numRuns):
         # Use random positions for each run to avoid caching effects
         start = random.randint(minChromPos, maxChromPos)
@@ -134,12 +185,28 @@ def benchmark_aggregate_count(numRuns, minChromPos, maxChromPos, query_length, c
             }}
         ]
 
-        startTime = datetime.datetime.now()
-        collection.aggregate(pipeline)
-        endTime = datetime.datetime.now()
-        ex_times.append((endTime - startTime).total_seconds())
+        ex_time, count = run_query("agg_count", collection, pipeline)
 
-    return ex_times
+        counts.append(count)
+        ex_times.append(ex_time)
+
+    return ex_times, counts
+
+
+def run_query(method, collection, query):
+    startTime = datetime.datetime.now()
+    if method == "find_count":
+        count = collection.find(query).count()
+    elif method == "agg_count":
+        count = collection.aggregate(query)
+    else:
+        print("Unrecognised method: {}".format(method))
+        sys.exit(1)
+    endTime = datetime.datetime.now()
+    ex_time = (endTime - startTime).total_seconds()
+
+    return ex_time, count
+
 
 
 def get_collection(uri=None, db=None, user=None, password=None, collection=None):
